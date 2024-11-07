e9013c2a9a8b35220c0e43d37ef9056a61ad220a
iduang <chnzqd@163.com>
2024-11-07 21:20:37 +0800

    [INIT] 初始化工程

---
+++ src/components/TheWelcome.vue
@@ -0,0 +1,16 @@
+<template>
+  <div>
+    <WelcomeItem>
+      <template #icon>
+        <DocumentationIcon />
+      </template>
+      <template #heading>Documentation</template>
+      get started.
+    </WelcomeItem>
+  </div>
+</template>
+
+<script setup lang="ts">
+import WelcomeItem from "./WelcomeItem.vue";
+import DocumentationIcon from "./icons/IconDocumentation.vue";
+</script>
